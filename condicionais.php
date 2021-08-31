989077b886061512602ab62d400003615cfc7b72
José Arnaldo <josearnaldo.net@gmail.com>
2021-08-31 15:07:00 -0300

    Update condicionais.php
    Quem não usa condicional simples que atire a primeira pedra

--- condicionais.php
+++ condicionais.php
@@ -13,5 +13,5 @@ echo "<p>";
 echo ($num1 > $num2) ? "Menor" : "Maior"; //Ternario
 echo "</p>";
 
-if ($num1 < $num2) 
+if ($num1 < $num2)
     echo "<p>" . $num1 . " é menor que " . $num2 . "</p>";
