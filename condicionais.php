ab766bfc92caf2099670f121fa73817460148edc
José Arnaldo <josearnaldo.net@gmail.com>
2021-08-31 15:05:10 -0300

    Update condicionais.php

--- condicionais.php
+++ condicionais.php
@@ -1,1 +1,17 @@
+<?php
+$string = "VidaLonga";
+$num1   = 1;
+$num2   = 2;
 
+echo $string ? "True" : "False"; //Ternario
+
+echo "<p>";
+echo ($num1 < $num2) ? "Menor" : "Maior"; //Ternario
+echo "</p>";
+
+echo "<p>";
+echo ($num1 > $num2) ? "Menor" : "Maior"; //Ternario
+echo "</p>";
+
+if ($num1 < $num2) 
+    echo "<p>" . $num1 . " é menor que " . $num2 . "</p>";
