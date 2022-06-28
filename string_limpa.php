7e770d3e5a216064ed61cd3db0e0358f34573a92
José Arnaldo <josearnaldo.net@gmail.com>
2022-06-28 18:34:45 -0300

    Adição de String Limpa

---
+++ string_limpa.php
@@ -0,0 +1,5 @@
+<?php
+//Fonte: https://blog.dbins.com.br/removendo-acentos-e-caracteres-especiais-com-php
+$string = "Indònésia";
+$string_limpa = preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
+echo $string_limpa;
