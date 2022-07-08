8c7151dc566dd2554d254f34e0e3ef6cf5f8a091
José Arnaldo <josearnaldo.net@gmail.com>
2022-07-08 00:02:39 -0300

    Add files via upload

--- palindromo.php
+++ palindromo.php
@@ -1,10 +1,14 @@
 <?php
-$string = "Morram após a sopa marrom";
-$string = strtolower($string);
-$string = preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
-$palindromo = strrev($string);
-if ($string == $palindromo) {
-    echo "Palindromo de: " . $string . " é " . $palindromo;
-} else {
-    echo "Não há palindromo em: " . $string;
+function polindroma($palavra) {
+    $palavra = preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$palavra);
+    $invertida = strrev($palavra);
+    $teste = $invertida == $palavra ? 1 : 0;
+    return $teste;
 }
+
+echo polindroma("omissíssimo") . PHP_EOL;
+echo polindroma("sopapos") . PHP_EOL;
+echo polindroma("osso") . PHP_EOL;
+echo polindroma("jose") . PHP_EOL;
+echo polindroma("ovo") . PHP_EOL;
+echo polindroma("rasar") . PHP_EOL;
