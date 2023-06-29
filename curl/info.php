13e299880053559423880535439b0045a6306bb9
José Arnaldo <josearnaldo.net@gmail.com>
2023-06-29 19:21:42 -0300

    Update info.php

--- curl/info.php
+++ curl/info.php
@@ -6,7 +6,6 @@ function info($url) {
     curl_setopt( $ch, CURLOPT_URL, $url );
     curl_setopt( $ch, CURLOPT_HEADER, 1);
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    // curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
     curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
 
     $content = curl_exec( $ch );
